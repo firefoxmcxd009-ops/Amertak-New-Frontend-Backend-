09538d1f05320307c702fb59519fe371550274ec
firefoxmcxd009-ops <firefoxmcxd009@gmail.com>
2026-06-25 16:42:49 +0700

    update

--- public/progress-loader/progress.js
+++
@@ -1,7 +0,0 @@
-const loader = document.getElementById("loader")
-const text = document.getElementById("range_label")
-const range = document.getElementById('range');
-
-range.addEventListener("animationend", () => {
-    loader.classList.toggle('endloader');
-});
